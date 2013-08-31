580f89b664f4a7af1579233c0d41fb6e7fad6488
foxymophoandlemama <ilgloriosodfavero@gmail.com>
2013-08-31 16:00:04 +0200

    First try with selects and .js needs implementation

---
+++ flask-workout/app/static/js/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$('.context-select').onclick(function(){
+		return false;
+	});
+});
--- flask-workout/app/views.py
+++ flask-workout/app/views.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 from database import db_session
-from models import Product
+from models import Product, Category, ExpenseContext
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
@@ -18,5 +18,13 @@ def shutdown_session(exception=None):
 
 @app.route('/')
 def index():
-    products = Product.query.all()
-    return render_template('index.html', products=products)
+    contexts = ExpenseContext.query.all()
+    return render_template('index.html', contexts=contexts)
+
+@app.route('/get_categories_by_context')
+def get_categories_by_context(context_id):
+    categories = Category.query.filter(Category.context_id==context_id).all()
+    return render_template('index.html', categories=categories)
+    
+    
+    
